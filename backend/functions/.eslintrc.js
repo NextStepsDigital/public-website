7cc9fa54cf28bd5c5da3b6770e0f08597d3c7051
Ben N <ben.n@dev.net>
2025-01-02 16:49:17 +0000

    update linting

--- backend/functions/.eslintrc.js
+++ backend/functions/.eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     "import",
   ],
   rules: {
+    "max-len": ["error", {"code": 100, "tabWidth": 2, "ignoreUrls": true}],
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
